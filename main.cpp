f7f2cd105ae5c59c4ba2f3e9ba199bf35dd0f171
BlackWoodik <100531159+BlackWoodik@users.noreply.github.com>
2022-05-24 14:27:09 +0300

    Shag-2 - end

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ input_numbers(size_t count) {
     }
     return result;
 }
+
 void
 find_minmax(const vector<double>& numbers, double& min, double& max) {
     min = numbers[0];
@@ -40,8 +41,8 @@ make_histogram(const vector<double>& numbers, size_t bin_count, double min, doub
 
 void
 show_histogram_text(const auto bins){
-const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+    const auto SCREEN_WIDTH = 80;
+    const auto MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
     size_t max_count = 0;
     for (size_t count : bins) {
@@ -74,6 +75,50 @@ const size_t SCREEN_WIDTH = 80;
 
 }
 
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='"<< baseline<< "'>"<<text<<"</text>";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke, string fill){
+    cout<< "<rect x= '" << x <<"' y='"<< y << "' width='"<<width<<"' height='" << height << "' stroke='"<< stroke<<"' fill='"<<fill<<"' />\n ";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(400, 300);
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "black", "black");
+        top += BIN_HEIGHT;
+}
+    svg_end();
+}
+
 int main() {
     // ¬вод данных
     size_t number_count;
@@ -94,7 +139,7 @@ int main() {
 
     const auto bins = make_histogram(numbers, bin_count, min, max);
 
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
     return 0;
 }
